2c6c146bfa2098179ee362134b5f138875b5c312
Fkame <nikitos_flame@mail.ru>
2022-04-24 12:11:14 +0300

    v0.2.1 - add jlink runtime image generation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module app {
     requires javafx.controls;
     requires javafx.fxml;
-    requires exp4j;
+    //requires exp4j;
     opens app.controllers to javafx.fxml;
     opens app.controllers.areaFinder1d to javafx.fxml;
     opens app.controllers.areaFinder2d to javafx.fxml;
